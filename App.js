1c7b3ee5ffda23558a5a8786746033b36b796526
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2019-12-20 14:36:03 +0100

    wip

--- App.js
+++ App.js
@@ -1,21 +1,22 @@
-import React, { useState }from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
-  const [outputText, setOutputText] = useState('Open up App.js to start working on your app!')
   return (
-    <View style={styles.container}>
-<Text>{outputText}</Text>
-      <Button title="Change Text" onPress={() => setOutputText('The text Changed')}/>
+    <View style={{padding: 30}}>
+      <View>
+        <TextInput placeholder="Enter yours" style={{borderColor: 'grey', borderBottom: 1, padding: 10}}/>
+      <Button title="ADD"/>
+    </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,  
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
